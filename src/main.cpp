be7f32f5c78ee72e3e9fc337161d74753a01f030
Stanislaumich <staslz42stas@gmail.com>
2019-11-02 01:24:53 +0300

    Добавлен показ времени

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,9 @@
 #define VFD_BYTES_PER_DIGIT 3      //3
 PT6311 pt6311_driver;
 
+int f[10]={140,128,12,132,128,132,140,128,140,132};
+int s[10]={196,64,195,195,71,135,135,192,199,199};
+
 void write_raw(uint8_t value, uint8_t number_of_bytes) 
 {
     pt6311_driver.displayMemWriteCmd(true, false);
@@ -36,6 +39,54 @@ void fill_mem(uint8_t value, uint8_t number_of_bytes, uint8_t start_addr)
 
     //And finally, switch on all segments for first three digits to make sure that display works.
     write_raw(value, number_of_bytes);
+}
+
+void showtime(int h, int m,int ss)
+{
+//18
+  pt6311_driver.addrSetCmd(18);
+  pt6311_driver.displayMemWriteCmd(true, false);
+  //pt6311_driver.data(0, false, false);
+  pt6311_driver.data(f[(h-h%10)/10]+0, false, false);
+  pt6311_driver.data(s[(h-h%10)/10], false, false);
+  pt6311_driver.data(0, false, true);
+//15
+pt6311_driver.addrSetCmd(15);
+  pt6311_driver.displayMemWriteCmd(true, false);
+  //pt6311_driver.data(0, false, false);
+  pt6311_driver.data(f[h%10]+2, false, false);
+  pt6311_driver.data(s[h%10], false, false);
+  pt6311_driver.data(0, false, true);
+//12
+pt6311_driver.addrSetCmd(12);
+  pt6311_driver.displayMemWriteCmd(true, false);
+  //pt6311_driver.data(0, false, false);
+  pt6311_driver.data(f[(m-m%10)/10]+0, false, false);
+  pt6311_driver.data(s[(m-m%10)/10], false, false);
+  pt6311_driver.data(0, false, true);
+//9
+pt6311_driver.addrSetCmd(9);
+  pt6311_driver.displayMemWriteCmd(true, false);
+  //pt6311_driver.data(0, false, false);
+  pt6311_driver.data(f[m%10]+2, false, false);
+  pt6311_driver.data(s[m%10], false, false);
+  pt6311_driver.data(0, false, true);
+//6
+pt6311_driver.addrSetCmd(6);
+  pt6311_driver.displayMemWriteCmd(true, false);
+  //pt6311_driver.data(0, false, false);
+  pt6311_driver.data(f[(ss-ss%10)/10]+0, false, false);
+  pt6311_driver.data(s[(ss-ss%10)/10], false, false);
+  pt6311_driver.data(0, false, true);
+//3
+pt6311_driver.addrSetCmd(3);
+  pt6311_driver.displayMemWriteCmd(true, false);
+  //pt6311_driver.data(0, false, false);
+  pt6311_driver.data(f[ss%10], false, false);
+  pt6311_driver.data(s[ss%10], false, false);
+  pt6311_driver.data(0, false, true);
+
+
 }
 
 void setup()
@@ -75,8 +126,8 @@ void setup()
   pt6311_driver.addrSetCmd(3);
   pt6311_driver.displayMemWriteCmd(true, false);
   //pt6311_driver.data(0, false, false);
-  pt6311_driver.data(8, false, false);
-  pt6311_driver.data(132, false, false);
+  pt6311_driver.data(140, false, false);
+  pt6311_driver.data(196, false, false);
   pt6311_driver.data(0, false, true);
   
  //pt6311_driver.displayLEDWriteCmd(true, false);
@@ -101,9 +152,9 @@ void loop()
       pt6311_driver.data(0, false, false);
       pt6311_driver.data(0, false, true);
         i/=2;
-        delay(100);
+        delay(200);
       }
-      
+      showtime(21,29,36);
       /*
       pt6311_driver.displayLEDWriteCmd(true, false);
       pt6311_driver.data(0, false, true);
